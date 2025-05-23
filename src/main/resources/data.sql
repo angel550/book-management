a3751cdc795c22d133e38f24b2c1048df889fae3
Luis Angel <113961846+angel550@users.noreply.github.com>
2025-05-23 17:25:05 -0600

    Fix sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO profile (name, description) VALUES ('Tech Fundamentals Profile', 'Profile with books about core skills to work in tech');
+INSERT INTO profile (name, description) VALUES ('Music Lover Profile', 'Profile with books about music history');
+
+INSERT INTO book (title, isbn, publication_year, author, publisher, status, profile_id)
+VALUES
+    ('Clean Code: A Handbook of Agile Software Craftsmanship', '9780132350884', 2008, 'Robert C. Martin', 'Prentice Hall', 'PLANNED', 1),
+    ('The Pragmatic Programmer: Your Journey to Mastery', '9780135957059', 2019, 'Andrew Hunt, David Thomas', 'Addison-Wesley', 'COMPLETED', 1),
+    ('Algorithms (4th Edition)', '9780321573513', 2011, 'Robert Sedgewick, Kevin Wayne', 'Addison-Wesley', 'COMPLETED', 1),
+    ('Sonic Life: A Memoir', '9780385548656', 2023, 'Thurston Moore', 'Doubleday', 'READING', 2),
+    ('This Is Your Brain on Music: The Science of a Human Obsession', '9780525949695', 2006, 'Daniel J. Levitin', 'Dutton Penguin', 'PAUSED', 2);
